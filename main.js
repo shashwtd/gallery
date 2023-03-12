2f5cef10ac2584b9348cbfcb7903b13874d499b2
cy-bug <shashwat5590@gmail.com>
2023-03-12 13:08:39 +0530

    remove lenis and jquery

--- main.js
+++ main.js
@@ -182,29 +182,6 @@ window.onload = () => {
 	setSize();
 	template = document.querySelector("#main").innerHTML;
 
-	// const lenis = new Lenis({
-	// 	duration: 1.2,
-	// 	easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-	// 	direction: "vertical", // vertical, horizontal
-	// 	gestureDirection: "vertical", // vertical, horizontal, both
-	// 	smooth: true,
-	// 	mouseMultiplier: 1,
-	// 	smoothTouch: false,
-	// 	touchMultiplier: 2,
-	// });
-
-	// //get scroll value
-	// lenis.on("scroll", ({ scroll, limit, velocity, direction, progress }) => {
-	// 	console.log({ scroll, limit, velocity, direction, progress });
-	// });
-
-	// function raf(time) {
-	// 	lenis.raf(time);
-	// 	requestAnimationFrame(raf);
-	// }
-
-	// requestAnimationFrame(raf);
-
 	activateListener();
 };
 
